2ea46dff7d5aca12ef88ad0942830684efb1f889
maniravirala <raviralamani5@gmail.com>
2024-08-12 15:30:01 +0530

    Going to initiate TanStack File Based Routing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { config } from 'dotenv';
+import { config } from "dotenv";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   define: {
     "process.env": process.env,
   },
