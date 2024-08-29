d5fdaf9dc3ce21c97e90d7d1760e6b68762b1127
maniravirala <raviralamani5@gmail.com>
2024-08-29 20:44:43 +0530

    feat: update resume template with drag and drop functionality
    Update the specialized2 resume template to include drag and drop functionality for rearranging sections.
    
    This change improves the user experience by allowing users to customize the order of sections in their resume more easily. It also makes the resume template more flexible and adaptable to different user needs.

--- Frontend/src/components/Resume/Templates/Sortable/SortableList.jsx
+++ Frontend/src/components/Resume/Templates/Sortable/SortableList.jsx
@@ -1,86 +1,86 @@
-/* eslint-disable react/prop-types */
-import {
-  DndContext,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  SortableContext,
-  sortableKeyboardCoordinates,
-  useSortable,
-  verticalListSortingStrategy,
-} from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
+// /* eslint-disable react/prop-types */
+// import {
+//   DndContext,
+//   closestCenter,
+//   KeyboardSensor,
+//   PointerSensor,
+//   useSensor,
+//   useSensors,
+// } from "@dnd-kit/core";
+// import {
+//   SortableContext,
+//   sortableKeyboardCoordinates,
+//   useSortable,
+//   verticalListSortingStrategy,
+// } from "@dnd-kit/sortable";
+// import { CSS } from "@dnd-kit/utilities";
 
-const SortableItem = ({ id, value }) => {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id });
+// const SortableItem = ({ id, value }) => {
+//   const { attributes, listeners, setNodeRef, transform, transition } =
+//     useSortable({ id });
 
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-  };
+//   const style = {
+//     transform: CSS.Transform.toString(transform),
+//     transition,
+//   };
 
-  return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      {...attributes}
-      {...listeners}
-      className="sortable-item w-full"
-    >
-      {value}
-    </div>
-  );
-};
+//   return (
+//     <div
+//       ref={setNodeRef}
+//       style={style}
+//       {...attributes}
+//       {...listeners}
+//       className="sortable-item w-full"
+//     >
+//       {value}
+//     </div>
+//   );
+// };
 
-const SortableList = ({ templateItems, onSortTemplateItems }) => {
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    }),
-  );
+// const SortableList = ({ templateItems, onSortTemplateItems }) => {
+//   const sensors = useSensors(
+//     useSensor(PointerSensor),
+//     useSensor(KeyboardSensor, {
+//       coordinateGetter: sortableKeyboardCoordinates,
+//     }),
+//   );
 
-  // const handleDragEnd = (event) => {
-  //   const { active, over } = event;
-  //   if (active.id !== over.id) {
-  //     setTemplateItems((items) => {
-  //       const oldIndex = items.findIndex((item) => item.id === active.id);
-  //       const newIndex = items.findIndex((item) => item.id === over.id);
-  //       return arrayMove(items, oldIndex, newIndex);
-  //     });
-  //   }
-  // };
+//   // const handleDragEnd = (event) => {
+//   //   const { active, over } = event;
+//   //   if (active.id !== over.id) {
+//   //     setTemplateItems((items) => {
+//   //       const oldIndex = items.findIndex((item) => item.id === active.id);
+//   //       const newIndex = items.findIndex((item) => item.id === over.id);
+//   //       return arrayMove(items, oldIndex, newIndex);
+//   //     });
+//   //   }
+//   // };
 
-  const handleDragEnd = (event) => {
-    const { active, over } = event;
-    if (active.id !== over.id) {
-      onSortTemplateItems(active.id, over.id);
-    }
-  };
+//   const handleDragEnd = (event) => {
+//     const { active, over } = event;
+//     if (active.id !== over.id) {
+//       onSortTemplateItems(active.id, over.id);
+//     }
+//   };
 
-  return (
-    <DndContext
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <SortableContext
-        items={templateItems}
-        strategy={verticalListSortingStrategy}
-      >
-        <div>
-          {templateItems.map((item) => (
-            <SortableItem key={item.id} id={item.id} value={item.component} />
-          ))}
-        </div>
-      </SortableContext>
-    </DndContext>
-  );
-};
+//   return (
+//     <DndContext
+//       sensors={sensors}
+//       collisionDetection={closestCenter}
+//       onDragEnd={handleDragEnd}
+//     >
+//       <SortableContext
+//         items={templateItems}
+//         strategy={verticalListSortingStrategy}
+//       >
+//         <div>
+//           {templateItems.map((item) => (
+//             <SortableItem key={item.id} id={item.id} value={item.component} />
+//           ))}
+//         </div>
+//       </SortableContext>
+//     </DndContext>
+//   );
+// };
 
-export default SortableList;
+// export default SortableList;
